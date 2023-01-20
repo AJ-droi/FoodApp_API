ac670fc8bbbbaffd05b752e5ddf84095fb632541
Decagon <decagon@Decagons-MacBook-Air.local>
2023-01-20 10:03:56 +0100

    foodapp cors

--- src/app.ts
+++ src/app.ts
@@ -22,7 +22,13 @@ db.sync().then(() => {
 
 const app = express()
 
-app.use(cors());
+const corsOptions = {
+    origin: 'https://foodorderingapp.onrender.com',
+    optionsSuccessStatus: 200 // some legacy browsers (IE11, various SmartTVs) choke on 204
+  }
+  
+
+app.use(cors(corsOptions));
 
 app.use(express.json())
 app.use(express.urlencoded({ extended: true }));
